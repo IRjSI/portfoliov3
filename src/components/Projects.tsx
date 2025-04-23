1a91a58eb962bfbc9a03dba3e3958f1b17bf9d4e
IRjSI <rajusuthar0077@gmail.com>
2025-04-23 16:47:42 +0530

    Fix image paths in Projects component for correct rendering

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -13,11 +13,11 @@ const Projects = () => {
         <p className="text-[#a1a1a1] font-medium">● Projects</p>
         <p className="text-green-500 bg-green-500/20 px-2 rounded-lg">• live</p>
       </div>
-      <ProjectCard link="https://rjsv3.vercel.app" img="public\Screenshot 2025-04-23 161938.png" name="Portfolio v3" description="A modern and sleek Portfolio website" />
-      <ProjectCard link="https://note-cove.vercel.app" img="public\Screenshot 2025-04-19 111923.png" name="Note-Cove" description="A note taking app with markdown support" />
-      <ProjectCard link="https://rjsv2.vercel.app" img="public\Screenshot 2025-04-19 111354.png" name="Portfolio v2" description="A modern and sleek portfolio" />
-      <ProjectCard link="https://book-trakker.vercel.app" img="public\Screenshot 2025-04-23 161807.png" name="Book Tracker" description="A book tracker web-app" />
-      <ProjectCard link="/" img="public\Screenshot 2025-01-17 161510.png" name="Movie Finder" description="A website to find about your favourite movies" />
+      <ProjectCard link="https://rjsv3.vercel.app" img="/screenshot-2025-04-23-161938.png" name="Portfolio v3" description="A modern and sleek Portfolio website" />
+      <ProjectCard link="https://note-cove.vercel.app" img="/Screenshot 2025-04-19 111923.png" name="Note-Cove" description="A note taking app with markdown support" />
+      <ProjectCard link="https://rjsv2.vercel.app" img="/Screenshot 2025-04-19 111354.png" name="Portfolio v2" description="A modern and sleek portfolio" />
+      <ProjectCard link="https://book-trakker.vercel.app" img="/Screenshot 2025-04-23 161807.png" name="Book Tracker" description="A book tracker web-app" />
+      <ProjectCard link="/" img="/Screenshot 2025-01-17 161510.png" name="Movie Finder" description="A website to find about your favourite movies" />
       <p className="text-center">...</p>
     </motion.div>
   )
